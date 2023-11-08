ddc91a5cded87d8c0316feea59f937e28df8f233
ethan <wsvn53@gmail.com>
2023-11-08 20:50:49 +0800

    1. Refactor the code to support reading token and downloading private models; 2. Remove jq dependency, support using Python to parse JSON.

--- README.md
+++ README.md
@@ -1,31 +1,37 @@
 # hugging-dl
 
-Simple script to download model files on HuggingFace. The only dependencies are curl/awk.
+Simple script to download model files on HuggingFace. The only dependencies are curl.
 
 ## Background
 
 Downloading models from Huggingface can be frustrating. Git clone relies on `git-lfs` and takes up more storage space after completion (due to git commit history). Models downloaded using the transformers API are stored as `soft links` in the cache directory, which is not conducive to sharing. Therefore, this small tool was developed that analyzes the download address of the model on the Huggingface page, downloads all its files separately, and supports breakpoint resumption and retrying downloads to avoid wasting network bandwidth and time due to unexpected disconnections during model downloading.
 
+## Features
+
+1. Download model/dataset files from Huggingface without git lfs for saving your storage space.
+2. Supports downloading your private models from Huggingface with an access token.
+3. Continuously resume download progress of the model files after connection aborted.
+4. Supports downloading models from a Huggingface mirror site URL.
+
 ## Usage
-
-This tool required `jq` for parsing json data, so please install via:
-
-```sh
-# Ubuntu or Debian
-sudo apt install jq
-
-# For macOS
-brew install jq
-```
 
 ```sh
 ./hugging-dl.sh -h
 
-usage: hugging-dl.sh [model_path|model_url] [output_dir]
+Usage: hugging-dl.sh [model_path|model_url] [output_dir]
 
-1. You can directly copy the [model_path] from the model card page, such as [mosaicml/mpt-30b-instruct].
-2. The [model_url] is full URL of the model such as [https://huggingface.co/mosaicml/mpt-30b-instruct].
-3. The [output_dir] is the path to save the files of model, if not specificed the default path will be the name of the [model_path].
+Options:
+  -h, --help: Print help message and exit.
+  -v, --version: Print version and exit.
+  -t, --token: Set Huggingface access token for authentication, which can be created from [https://huggingface.co/settings/tokens]
+
+Example:
+  hugging-dl.sh meta-llama/Llama-2-70b-chat-hf /path/to/save/model
+  hugging-dl.sh https://huggingface.co/meta-llama/Llama-2-70b-chat-hf /path/to/save/model
+
+1. You can directly copy the [model_path] from the model card page, such as [meta-llama/Llama-2-70b-chat-hf].
+2. The [model_url] is full URL of the model such as [https://huggingface.co/meta-llama/Llama-2-70b-chat-hf].
+3. The [output_dir] is the path to save the files of model, if not specified the default path will be the name of the [model_path].
 
 For more information, please visit: https://github.com/wsvn53/hugging-dl
 ```
@@ -36,6 +42,7 @@ Examples:
 hugging-dl.sh gpt2
 hugging-dl.sh https://huggingface.co/mosaicml/mpt-7b-chat
 hugging-dl.sh https://huggingface.co/mosaicml/mpt-7b-chat mpt-7b-chat
+hugging-dl.sh -t hf_sctiSbZiVL..... https://huggingface.co/meta-llama/Llama-2-70b-chat-hf
 ```
 
 ## License
--- hugging-dl.sh
+++ hugging-dl.sh
@@ -1,59 +1,149 @@
 #!/bin/bash
 
-[[ $1 == '-h' ]] && {
-	echo "usage: hugging-dl.sh [model_path|model_url] [output_dir]";
-	echo "";
-	echo "1. You can directly copy the [model_path] from the model card page, such as [mosaicml/mpt-30b-instruct]."
-	echo "2. The [model_url] is full URL of the model such as [https://huggingface.co/mosaicml/mpt-30b-instruct]."
-	echo "3. The [output_dir] is the path to save the files of model, if not specificed the default path will be the name of the [model_path]."
-	echo "";
-	echo "For more information, please visit: https://github.com/wsvn53/hugging-dl"
-	exit 0;
+VERSION=0.1
+
+# Read all options from command line, supported options:
+# 1. -h, --help: Print help message and exit.
+# 2. -v, --version: Print version and exit.
+# r. -t, --token: Set Huggingface access token for authentication, which can be created from [https://huggingface.co/settings/tokens].
+hugging_url_or_path="";
+path_to_save_model="";
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h|--help)
+            echo "Usage: hugging-dl.sh [model_path|model_url] [output_dir]";
+            echo "";
+            echo "Options:";
+            echo "  -h, --help: Print help message and exit.";
+            echo "  -v, --version: Print version and exit.";
+            echo "  -t, --token: Set Huggingface access token for authentication, which can be created from [https://huggingface.co/settings/tokens]";
+            echo "";
+            echo "Example:"
+            echo "  hugging-dl.sh meta-llama/Llama-2-70b-chat-hf /path/to/save/model";
+            echo "  hugging-dl.sh https://huggingface.co/meta-llama/Llama-2-70b-chat-hf /path/to/save/model";
+            echo "";
+            echo "1. You can directly copy the [model_path] from the model card page, such as [meta-llama/Llama-2-70b-chat-hf]."
+            echo "2. The [model_url] is full URL of the model such as [https://huggingface.co/meta-llama/Llama-2-70b-chat-hf]."
+            echo "3. The [output_dir] is the path to save the files of model, if not specified the default path will be the name of the [model_path]."
+            echo "";
+            echo "For more information, please visit: https://github.com/wsvn53/hugging-dl"
+            exit 0;
+            ;;
+        -v|--version)
+            echo "hugging-dl version: $VERSION";
+            exit 0;
+            ;;
+        -t|--token)
+            token=$2;
+            shift;
+            shift;
+            ;;
+        *)
+            # The first free option is the model url or repo path
+            hugging_url_or_path=$1;
+            shift;
+            # The second free option is the output dir
+            path_to_save_model=$1;
+            shift;
+            ;;
+    esac
+done
+
+# Required jq or python to parse json
+which jq>/dev/null || which python3 2>/dev/null || {
+    echo "[ERROR] No 'jq' and 'python3' command found, please install 'jq' or 'python3' first!";
+    exit 1;
 }
 
-# Required jq
-which jq > /dev/null || {
-	echo "🚫 No 'jq' command found, please install jq first!";
-	exit 1002;
-}
+# Read first free option as model url
+[[ -z "$hugging_url_or_path" ]] && echo "[FAILED] No huggingface model url or repo path specified!" && exit 2;
+[[ "$hugging_url_or_path" != http* ]] && hugging_url_or_path="https://huggingface.co/$hugging_url_or_path";
+echo "🤗 Starting download Huggingface model from url: $hugging_url_or_path";
 
-hugging_url=$1;
-[[ -z "$hugging_url" ]] && echo "🚫 No huggingface model url specificed!" && exit 1000;
-[[ "$hugging_url" != http* ]] && hugging_url="https://huggingface.co/$hugging_url";
-
-hugging_host="$(echo "$hugging_url" | cut -d/ -f1)/$(echo "$hugging_url" | cut -d/ -f2)/$(echo "$hugging_url" | cut -d/ -f3)";
-echo "ℹ️ Huggingface host: $hugging_host";
-
-# Auto download HuggineFace files with curl
-hugging_api_prefix="$hugging_host/api/models";
+# Detect Huggingface host [huggingface.co] for assemble API url
+hugging_proto=$(echo "$hugging_url_or_path" | cut -d/ -f1);
+hugging_host=$(echo "$hugging_url_or_path" | cut -d/ -f3);
+hugging_api_prefix="$hugging_proto//$hugging_host/api/models";
 hugging_api_suffix="tree/main";
+echo "→ Using Huggingface API prefix: $hugging_api_prefix/../$hugging_api_suffix";
 
-output_dir=$2;
-[[ -z "$output_dir" ]] && output_dir=$(basename $hugging_url);
+# Detect path to save model files
+[[ -z "$path_to_save_model" ]] && path_to_save_model=$(basename "$hugging_url_or_path");
+echo "→ Path to save model files: $path_to_save_model";
+[[ -d "$path_to_save_model" ]] || mkdir -p "$path_to_save_model";
+cd "$output_dir" || {
+    echo "[ERROR] Can't change directory to $path_to_save_model, please check permissions.";
+    exit 3;
+}
 
-[[ -d "$output_dir" ]] || mkdir -pv "$output_dir";
-cd "$output_dir";
+# Detect model path from huggingface url
+# shellcheck disable=SC2001
+hugging_model_root_path="$(echo "$hugging_url_or_path" | sed "s#$hugging_proto//$hugging_host/##g")";
+hugging_model_api_url="$hugging_api_prefix/$hugging_model_root_path/$hugging_api_suffix";
+echo "→ Huggingface API url for $hugging_model_root_path: $hugging_model_api_url";
 
-hugging_path="$(echo "$hugging_url" | sed "s#$hugging_host/##g")";
-hugging_url="$(echo "$hugging_url" | sed "s#$hugging_host#$hugging_api_prefix#g")/$hugging_api_suffix";
+# Function to parse value from json
+parse_json_value() {
+    local json_data=$1;
+    local key=$2;
 
-echo "ℹ️ Huggingface API URL: [$hugging_url]";
-echo "ℹ️ Huggingface Model Path: [$hugging_path]";
+    # Using jq to parse json if installed
+    which jq>/dev/null && echo "$json_data" | jq ".$key" --raw-output --compact-output 2>/dev/null && return 0;
+    # Alternatively, use python3 to parse path
+    python -c "import json; import sys; data=json.loads('$json_data'); print(data['$key']) if '$key' != '[]' else [print(item) for item in data]"  2>/dev/null;
+    # shellcheck disable=SC2181
+    [[ $? != 0 ]] && printf "[ERROR] Load model files FAILED, please check the JSON data:\n%s\n" "$json_data" 1>&2 && \
+        printf "\n[Note] If you are downloading a private model, you may need to set an access token for downloading which can be created from [https://huggingface.co/settings/tokens].\n" 1>&2;
+}
 
-curl "$hugging_url" | jq '.[].path' --raw-output | while read filename; do
-	echo "⬇️ Downloading [$filename] ..";
-	down_url="$hugging_host/$hugging_path/resolve/main/$filename";
-	echo "- URL: $down_url";
-	max_retry=20;
-	ret_code=1;
-	while [[ $ret_code != 0 ]]; do 
-		echo "curl -C - -L -O \"$down_url\""; 
-		curl -C - -L -O "$down_url"; 
+# Function to download file
+download_file() {
+    local filename=$1;
+	local file_url="$hugging_proto//$hugging_host$hugging_path/$hugging_model_root_path/resolve/main/$filename";
+
+	echo "⬇️Downloading [$filename] from [$file_url]";
+	local max_retry=20;
+	local ret_code=1;
+	while [[ $ret_code != 0 ]]; do
+		echo "→ curl -C - -L -o '$filename' \"$file_url\"";
+		curl -C - -H "Authorization: Bearer $token" -L -o "$filename" "$file_url";
 		ret_code=$?;
 		[[ $ret_code == 0 ]] && echo "✅ Download Completed [$filename]" && break;
 
 		max_retry=$((max_retry-1));
-		[[ $max_retry == 0 ]] && echo "🚫 Download still FAILED: $down_url" && break;
+		[[ $max_retry == 0 ]] && echo "[ERROR] Download failed: $file_url" && break;
 		echo "⚠️ Download failed. Retrying with continuous download(retry $max_retry remains)."
+		sleep 3;
 	done
-done
+}
+
+# Function to read download path from huggingface api url
+download_model_files() {
+    local current_path=$1;
+    local model_api_url="$hugging_model_api_url";
+    [[ ! -z "$current_path" ]] && {
+        model_api_url="$hugging_model_api_url$current_path";
+        echo "→ Downloading: [$current_path] from [$model_api_url]";
+        [[ ! -d ".$current_path" ]] && mkdir -p ".$current_path";
+    }
+    local model_data=$(curl -s -H "Authorization: Bearer $token" "$model_api_url");
+    # shellcheck disable=SC2162
+    parse_json_value "$model_data" "[]" | while read json_line; do
+        local file_type=$(parse_json_value "$json_line" "type");
+        local file_path=$(parse_json_value "$json_line" "path");
+
+        [[ "$file_type" == "directory" ]] && {
+            # Download files in sub directory
+            download_model_files "$current_path/$file_path";
+        }
+
+        [[ "$file_type" == "file" ]] && {
+            # Download model file
+            download_file "$file_path";
+        }
+    done
+}
+
+# Starting download
+cd "$path_to_save_model";
+download_model_files;
